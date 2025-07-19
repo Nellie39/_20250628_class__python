f4b7a3bcdd61893191d1a2b46dfd09013ad3f0c6
nellie <jobnellie39@gmail.com>
2025-07-19 16:04:16 +0800

    0719第四次變更

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -1,3 +1,16 @@
 # list
 fruits = ("蘋果", "banana", "櫻桃", "pear")
+fruits.append("mango")  #新增一個元素
+fruits.append("berry")  #再新增一個
+fruits.append("orange")
+fruits.append("grape")
+fruits.append("watermelon")
+fruits.append("pineapple")
+fruits.append("kiwi")
+fruits.append("lemon")
+fruits.append("papaya")
+fruits.append("peach")
+fruits.append("plum")
+fruits.append("strawberry")
+print(type(fruits))
 print("水果清單", fruits)
