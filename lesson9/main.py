67989b98735373777622d88836418162cad3fb22
nellie <jobnellie39@gmail.com>
2025-08-23 11:02:21 +0800

    0823 第二次變更

--- lesson9/main.py
+++ lesson9/main.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def helloe_world():
-    return "<p>Hello, World!</p>"
+@app.route("/")
+def index():
+    return "<p>Hello, Flask!</p>"
+
+@app.route("/name")
+def my_name():
+    return "<h1>Nellie.</h1>"
